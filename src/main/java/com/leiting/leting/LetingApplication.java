728fa5143ba83226c60cf54b444d066c658c8c6a
MasterYexl <43309271+MasterYexl@users.noreply.github.com>
2019-09-16 09:12:50 +0800

    Initial commit

---
+++ src/main/java/com/leiting/leting/LetingApplication.java
@@ -0,0 +1,13 @@
+package com.leiting.leting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetingApplication.class, args);
+    }
+
+}
